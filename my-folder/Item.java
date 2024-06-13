19be77f46191a7ac855d3e606b237eaf894ed60e
elinagrisule <165931608+elinagrisule@users.noreply.github.com>
2024-06-13 22:14:30 +0200

    Create Item.java

---
+++ my-folder/Item.java
@@ -0,0 +1,32 @@
+public class Item{
+  private int id;
+  private String name;
+  private int cost;
+
+  public Item(int id, String name, int cost){
+    this.id = id;
+    this.name = name;
+    this.cost = cost; 
+  }
+
+  public int getId(){
+    return id;
+  }
+
+  public void setName(String name){    
+    this.name = name;
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public void setCost(int cost){
+    // Check whether or not the user is a cashier, and if it is, then allow to change the cost.
+    this.cost = cost;
+  }
+
+  public int getCost(){
+    return cost;
+  }
+}
